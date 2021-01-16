7f156a4d7fe3e7050d8b0cb9eced4190858a5880
Exordium_And_Terminus <merrittsmax@gmail.com>
2021-01-15 20:10:40 -0500

    Initial Commit

---
+++ Shapely-OCC-Interchange/OCC_To_Shapely.py
@@ -0,0 +1,75 @@
+import collections
+from shapely.geometry import Polygon
+from OCC.Core.BRepTools import BRepTools_WireExplorer, breptools_OuterWire
+from OCC.Extend.TopologyUtils import discretize_edge, discretize_wire, TopologyExplorer
+from OCC.Core.TopExp import TopExp_Explorer
+from OCC.Core.TopAbs import TopAbs_FACE, TopAbs_EDGE, TopAbs_WIRE, TopAbs_INTERNAL, TopAbs_SHAPE
+from OCC.Core.TopoDS import TopoDS_Face
+
+#TODO: fix up type hinting for this file!!
+#Functions for converting back and forth between Shapely and Python OCC
+
+#convert to list for ease of use afterward
+def orderedDict_To_List(oDict):
+    edges = []
+    for item in oDict.keys():
+        edges.append(item)
+    return edges
+
+def getPointsFromWire(wire):
+    edgesDict = collections.OrderedDict()
+    previous = 0
+    wireExp = BRepTools_WireExplorer(wire)
+    while wireExp.More():
+        temp = discretize_edge(wireExp.Current())
+        if previous != 0:
+        #check to make sure the shared vertex coordinate is first, reverse if not (arcs have this issue)
+            if previous != temp[0]:
+                temp2=temp
+                temp2.reverse()
+                temp = temp2
+                previous = temp[-1]
+        else: previous = temp[-1]
+
+    #add the coordinates in order to the list, ignore value as we only care about the key
+        for coord in temp:
+            edgesDict[coord] = 0
+        wireExp.Next()
+    points = orderedDict_To_List(edgesDict)
+    return points
+
+# method to take in a face to make a Shapely polygon
+def getPointsFromFace(face):
+    holes = []
+    wiresToDiscretize = TopExp_Explorer(face, TopAbs_WIRE)
+    #if face has no holes, retrieve only wire and grab points
+    wiresToDiscretize.Next()
+    noHoles = not wiresToDiscretize.More()
+    if noHoles:
+        #reinitialize the explorer since we can't go back...
+        wiresToDiscretize = TopExp_Explorer(face, TopAbs_WIRE).Current()
+        points = getPointsFromWire(wiresToDiscretize)
+        return points
+    
+    #need to figure out which is outline and which are holes, discretize all wires first
+    else:
+        outerWire = breptools_OuterWire(face)
+        shape = getPointsFromWire(outerWire)
+
+        wiresToDiscretize = TopExp_Explorer(face, TopAbs_WIRE)
+        while wiresToDiscretize.More():
+            holes.append(getPointsFromWire(wiresToDiscretize.Current()))
+            wiresToDiscretize.Next()
+        
+        #remove the outerwire from the hole collection - once found, break the loop
+        for item in holes:
+            if item[0] == shape[0]:
+                holes.remove(item)
+                break
+
+        return shape, holes
+
+def Convert(face:TopoDS_Face) -> Polygon:
+    outline, holes = getPointsFromFace(face)
+    poly = Polygon(outline, holes)
+    return poly
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='Shapely-OCC-Interchange',
+    version='0.0.1',
+    author='Maximillian Merritts',
+    author_email='merrittsmax@gmail.com',
+    packages=[],
+    py_modules=[],
+    scripts=[],
+    url='https://github.com/ExordiumAndTerminus/Shapely-OCC-Interchange',
+    license='LICENSE.txt',
+    description='An un-described package.',
+    long_description=open('README.md').read(),
+    install_requires=[
+        
+    ],
+    python_requires='~=3.7'
+)
